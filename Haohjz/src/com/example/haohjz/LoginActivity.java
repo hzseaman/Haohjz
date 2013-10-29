3e6e02310ff16ff4712c2d3f3df6127a58d2c683
yxc <yinxiucheng_123@163.com>
2013-10-29 23:57:40 +0800

    好好记账 10月30日晚上提交

---
+++ Haohjz/src/com/example/haohjz/LoginActivity.java
@@ -0,0 +1,61 @@
+package com.example.haohjz;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.Window;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends Activity {
+
+	private EditText usernameEditText;
+	private EditText psdEditText;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.login);
+		usernameEditText = (EditText)findViewById(R.id.login_usname);
+		psdEditText = (EditText) findViewById(R.id.login_psd);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	public void click(View view){
+		switch (view.getId()) {
+		case R.id.login_ok:
+		{
+			if (200 == login()) {
+				Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+				startActivity(intent);
+			}else {
+				Toast.makeText(LoginActivity.this, "错误的账号或密码", Toast.LENGTH_LONG).show();
+			}
+			break;
+		}	
+		case R.id.login_cancel:
+		{
+			finish();
+			break;
+		}
+
+		default:
+			break;
+		}
+	}
+
+	public int login()
+	{
+		if (usernameEditText.getText().toString().equals("yxc")) {
+			return 200;
+		}else{
+			return 404;
+		}
+	}
+}
