aa666a9317dd643e301b13b1525429af708d5a41
yxc <yinxiucheng_123@163.com>
2013-10-29 23:57:40 +0800

    submit  code

---
+++ Haohjz/src/com/example/haohjz/RegisterActivity.java
@@ -0,0 +1,18 @@
+package com.example.haohjz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class RegisterActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.register);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		return super.onCreateOptionsMenu(menu);
+	}
+}
